815ae768c6f4724c0b59b17382434e6ca06893e3
Trinity <110600575+KateKeim@users.noreply.github.com>
2023-08-27 15:40:21 +0200

    Add exercise1.4 files

---
+++ Exercise1.4/Part1/recipe_input.py
@@ -0,0 +1,70 @@
+# Import the pickle module
+import pickle
+
+# Define a function called take_recipe() to take recipes from the user
+def take_recipe():
+    name = str(input('Enter the name of the recipe: '))
+    cooking_time = int(
+        input('Enter the cooking time of the recipe in minutes: ')
+    )
+    ingredients = input('Enter the ingredients of the recipe: ')
+    ingredients = ingredients.split()
+    ingredients = [ingredient.lower() for ingredient in ingredients]
+    recipe = {'Name': name, 'Cooking_Time': cooking_time, 'Ingredients': ingredients}
+    difficulty = calc_difficulty(recipe)
+    return recipe
+
+# Define the function calc_diffficulty(), where the difficulty is returned as Easy, Medium, Intermediate or Hard
+def calc_difficulty(recipe):
+    if recipe['Cooking_Time'] < 10 and len(recipe['Ingredients']) < 4:
+        recipe['Difficulty'] = 'Easy'
+
+    elif recipe['Cooking_Time'] < 10 and len(recipe['Ingredients']) >= 4:
+        recipe['Difficulty'] = 'Medium'
+
+    elif recipe['Cooking_Time'] >= 10 and len(recipe['Ingredients']) < 4:
+        recipe['Difficulty'] = 'Intermediate'
+
+    elif recipe['Cooking_Time'] >= 10 and len(recipe['Ingredients']) >= 4:
+        recipe['Difficulty'] = 'Hard'
+
+# Have the user enter a filename, which would attempt to open a binary file in read mode.
+recipes_list = []
+all_ingredients = []
+
+filename = str(input('Enter a filename with your recipes: '))
+try:
+    recipes_file = open(filename, 'rb')
+    data = pickle.load(recipes_file)
+except FileNotFoundError:
+    print('File not found. Creating a new file.')
+    data = {'recipes_list': [], 'all_ingredients': []}
+except:
+    print('Unexpected error. Creating a new file. ')
+    data = {'recipes_list': [], 'all_ingredients': []}
+else:
+    recipes_file.close()
+finally:
+    recipes_list = data['recipes_list']
+    all_ingredients = data['all_ingredients']
+
+# Ask the user how many recipes they’d like to enter, and define a for loop that calls the take_recipe() function.
+num = int(input('How many recipes would you like to enter? '))
+
+for i in range(num):
+    recipe = take_recipe()
+    print(recipe)
+
+    for ingredient in recipe['Ingredients']:
+        if ingredient not in all_ingredients:
+            all_ingredients.append(ingredient)
+
+    recipes_list.append(recipe)
+
+# Gather the updated recipes_list and all_ingredients into the dictionary called data.
+data = {'recipes_list': recipes_list, 'all_ingredients': all_ingredients}
+
+# Open a binary file with the user-defined filename and write data to it using the pickle module.
+new_file_name = str(input('Enter a name for the new file.'))
+new_file_name = open(new_file_name, 'wb')
+pickle.dump(data, new_file_name)
---
+++ Exercise1.4/Part2/recipe_search.py
@@ -0,0 +1,60 @@
+# Import the pickle module.
+import pickle
+
+# Define a function to display a recipe called display_recipe(), 
+# which takes in one recipe (of the dictionary type) as an argument 
+# and prints all of its attributes including the recipe name, cooking time, ingredients, and difficulty.
+def display_recipe(recipe):
+    print('Name: ', recipe['Name'])
+    print('Cooking time in minutes: ', recipe['Cooking_Time'])
+    print('Ingredients: ', ', '.join(recipe['Ingredients']))
+    print('Difficulty: ', recipe['Difficulty'])
+
+
+# Define another function called search_ingredient() to search for an ingredient in the given data.
+def search_ingredient(data):
+    ingredients_list = data['all_ingredients']
+    indexed_ingredients_list = list(enumerate(ingredients_list, 1))
+
+    for ingredient in indexed_ingredients_list:
+        print('No.', ingredient[0], ' - ', ingredient[1])
+
+    try:
+        chosen_num = int(
+            input('Enter the corresponding number of chosen ingredient:   '))
+        index = chosen_num - 1
+        ingredient_searched = ingredients_list[index]
+        ingredient_searched = ingredient_searched.lower()
+    except IndexError:
+        print('The number you entered is not on the list.')
+    except:
+        print('An error occurred while finding your ingredient.')
+    else:
+        for recipe in data['recipes_list']:
+            for recipe_ing in recipe['Ingredients']:
+                if (recipe_ing == ingredient_searched):
+                    print('\nThe following recipe includes the searched ingredient: ')
+                    print('------------------------------------------------------')
+                    display_recipe(recipe)
+
+# Ask the user for the name of the file that contains your recipe data.
+filename = str(
+    input('Enter the filename where you\'ve stored your recipes:  '))
+
+# Use a try block to open the file, and then extract its contents into data using the pickle module.
+try:
+    recipes_file = open(filename, 'rb')
+    data = pickle.load(recipes_file)
+
+# For when the try block fails, add an except block to warn the user that the file hasn’t been found.
+except FileNotFoundError:
+    print('File doesn\'t exist in the current directory')
+    data = {'recipes_list': [], 'all_ingredients': []}
+
+except:
+    print('An unexpected error occurred')
+    data = {'recipes_list': [], 'all_ingredients': []}
+
+# Define an else block that calls search_ingredient() while passing data into it as an argument.
+else:
+    search_ingredient(data)                        
